46bad05a935796446354984d645d4ad9fc637ae3
NaveenVinayak S <27666222+NaveenVinayakS@users.noreply.github.com>
2021-10-04 11:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,6 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+   
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
